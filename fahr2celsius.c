9b374d21bdb1eb071a52fad26c1d188a25c97793
kba977 <kba977@163.com>
2014-05-11 21:03:22 +0800

    modify with stmbolic constants

--- fahr2celsius.c
+++ fahr2celsius.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
+
+#define LOWER 0     // lower limit to table
+#define UPPER 300   // upper limit
+#define STEP  20    // step size
+ 
 main()
 {
-	/*  当fahr= 0, 20, ..., 300时,分别
-    	打印华氏温度与摄氏温度对照表 */
+	/* print Fahrenheit-Celsius table */
 	float fahr, celsius;
-	int lower, upper, step;
-	
-	lower = 0;			// 温度表的下限
-	upper = 300;		// 温度表的上限
-	step = 20;			// 步长
-	
-	fahr = upper;
 	
 	printf("Fahr to Celsius!\n");
 	
-	while (fahr >= lower) {
+	fahr = UPPER;
+	while (fahr >= LOWER) {
 		celsius = (5.0/9.0) * (fahr-32);
 		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr - step;
+		fahr = fahr - STEP;
 	}
 }
+
 
